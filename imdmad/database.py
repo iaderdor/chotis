c448a59f33f65d4c52dee2f1fef70650184aa021
Ismael Aderdor <info@iaderdor.info>
2020-08-23 06:22:42 +0200

    feat: Implement add and get stations from database
    Now we can add stations to our database.

--- imdmad/database.py
+++ imdmad/database.py
@@ -186,3 +186,138 @@ class Database:
 
         return self.get_dataset_by_name(dataset_name)
 
+    def add_stations(self, stations_list):
+        """Given a list with stations data, adds them to the database
+
+        Parameters
+        ----------
+        stations : `list` with `dict` or a `dict`
+            ``"id"``
+                Station ID
+            ``"name"``
+                Name of the station
+            ``"element_type"``
+                Type of station
+            ``"x_utm"``
+                X UTM coordinate
+            ``"y_utm"``
+                Y UTM coordinate
+
+        Returns
+        -------
+        stations : `list` with `dict`
+            ``"id"``
+                Station ID
+            ``"name"``
+                Name of the station
+            ``"element_type"`
+                Type of station
+            ``"x_utm"``
+                X UTM coordinate
+            ``"y_utm"``
+                Y UTM coordinate
+            ``"created_at"``
+                When the station was added to the database
+            ``"updated_at"``
+                When the station was last updated in the database
+
+        """
+
+        if type(stations_list) == dict:
+            stations_list = [stations_list]
+
+        stations_tuple_list = []
+        timestamp = datetime.datetime.now()
+
+        for idx, station in enumerate(stations_list):
+            if not station["id"]:
+                raise Exception(
+                    'id in station_list item {} is empty'.format(idx))
+
+            if not station["name"]:
+                raise Exception(
+                    'name in station_list item {} is empty'.format(idx))
+            if not station["element_type"]:
+                raise Exception(
+                    'element_type in station_list item {} is empty'.format(idx)
+                )
+
+            if not station["x_utm"]:
+                raise Exception(
+                    'x_utm in station_list item {} is empty'.format(idx))
+            if not station["y_utm"]:
+                raise Exception(
+                    'y_utm in station_list item {} is empty'.format(idx))
+
+            station["id"] = int(station["id"])
+            station["x_utm"] = float(station["x_utm"])
+            station["y_utm"] = float(station["y_utm"])
+            station["timestamp_created_at"] = timestamp
+            station["timestamp_updated_at"] = timestamp
+
+            stations_tuple_list.append(
+                tuple((
+                    station["id"],
+                    station["name"],
+                    station["element_type"],
+                    station["x_utm"],
+                    station["y_utm"],
+                    station["created_at"],
+                    station["updated_at"]
+                ))
+            )
+
+        c = self.conn.cursor()
+        query = """
+        INSERT INTO stations(id, name, element_type, x_utm, y_utm, timestamp_created_at, timestamp_updated_at) VALUES(?,?,?,?,?,?,?);
+        """
+        c.executemany(query, stations_tuple_list)
+        self.conn.commit()
+        c.close()
+
+        return stations_list
+
+    def get_station_by_id(self, id):
+        """Given a name, returns a dataset
+
+        Parameters
+        ----------
+        name : int
+            id of the station to retrieve
+
+        Returns
+        -------
+        `dict`
+            ``"id"``
+                ID of the station
+            ``"name"``
+                name of the id
+            ``"element_type"``
+                Wether the stations ir a urban one or an M-30 one
+            ``"x_utm"``
+                Coordinate x in UTM system
+            ``"y-utm"``
+                Coordinate y in UTM system
+            ``"created_at"``
+                When the station was added to the database
+            ``"updated_at"``
+                When the station was last updated in the database
+        """
+        c = self.conn.cursor()
+        query = "SELECT * FROM stations where id = ?;"
+        c.execute(query, [id])
+
+        station = c.fetchone()
+
+        result = {}
+        if type(station) is not type(None):
+            result["id"] = station[0]
+            result["name"] = station[1]
+            result["element_type"] = station[2]
+            result["x_utm"] = station[3]
+            result["y_utm"] = station[4]
+            result["created_at"] = station[5]
+            result["updated_at"] = station[6]
+        c.close()
+
+        return result
