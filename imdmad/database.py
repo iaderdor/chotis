433a72f4a433265210973b2f580cf3506bb8467e
Ismael Aderdor <info@iaderdor.info>
2020-08-23 06:19:32 +0200

    feat: Add ADD and GET datasets from database
    In order to be able to later manage the datasets we are adding to the
    database, we add de posibility to add and retrieve them from our
    database.

--- imdmad/database.py
+++ imdmad/database.py
@@ -93,3 +93,96 @@ class Database:
         self.__create_stations()
         self.__create_counts()
         self.__create_daily_counts()
+
+    def get_dataset_by_name(self, name):
+        """Given a name, returns a dataset
+
+        Parameters
+        ----------
+        name : str
+         The name oof the dataset to retrieve.
+
+        Returns
+        -------
+        `list` of `dict`
+            ``"id"``
+                ID of the dataset
+            ``"name"``
+                name of the dataset
+            ``"last_modified"``
+                Last time the dataset was modified by Madrid
+            ``"created_at"``
+                When the dataset was added to the database
+            ``"updated_at"``
+                When the dataset was last updated in the database
+        """
+        c = self.conn.cursor()
+        query = "SELECT * FROM datasets where name LIKE ?;"
+        c.execute(query, [name])
+
+        results = []
+
+        for idx, result in enumerate(c.fetchall()):
+            results.append({})
+            results[idx]["id"] = result[0]
+            results[idx]["name"] = result[1]
+            results[idx]["last_modified"] = result[2]
+            results[idx]["created_at"] = result[3]
+            results[idx]["updated_at"] = result[4]
+
+        c.close()
+
+        return results
+
+    def add_dataset(self, dataset_name, last_modified):
+        """Add a dataset to the dataset tables.
+
+        Parameters
+        ----------
+        dataset_name : `str`
+        last_modified : `DateTime`
+
+        Returns
+        -------
+        `dict`
+            ``"id"``
+                ID of the dataset
+            ``"name"``
+                name of the dataset
+            ``"last_modified"``
+                Last time the dataset was modified by Madrid
+            ``"created_at"``
+                When the dataset was added to the database
+            ``"updated_at"``
+                When the dataset was last updated in the database
+        """
+
+        if not dataset_name:
+            raise Exception("The name of dataset can't be empty")
+        if not isinstance(dataset_name, str):
+            raise Exception("The name of Dataset must be a string, but a {} was provided ".format(
+                type(dataset_name)))
+
+            if not last_modified:
+                raise Exception("last_modified can't be empty")
+        if not isinstance(last_modified, datetime.datetime):
+            raise Exception(
+                "last_modified must be a datetime.datetime, but a {} was provided".format(
+                    type(datetime.datetime)
+                )
+            )
+
+            timestamp = datetime.datetime.now()
+
+        dataset = (dataset_name, last_modified, str(timestamp), str(timestamp))
+
+        c = self.conn.cursor()
+        query = """
+        INSERT INTO datasets(name, dataset_last_modified,timestamp_created_at,timestamp_updated_at) VALUES(?,?,?,?);
+        """
+        c.execute(query, dataset)
+        self.conn.commit()
+        c.close()
+
+        return self.get_dataset_by_name(dataset_name)
+
